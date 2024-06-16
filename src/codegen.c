3c919cebb8dc54b7b7d5aacdd29f500f9cdfb254
DiaSird <78157340+DiaSird@users.noreply.github.com>
2024-06-16 14:56:57 +0900

    feat: Add global variables

--- src/codegen.c
+++ src/codegen.c
@@ -53,7 +53,13 @@ static void gen_addr(Node *node) {
     // |   stack base   | <- %rbp == %rsp -> mov %rbp, %rsp
     // |   address MAX  | <- bottom
 
-    printf(" lea %d(%%rbp), %%rax\n", node->var->offset);
+    if (node->var->is_local) {
+      // Local variable
+      printf("  lea %d(%%rbp), %%rax\n", node->var->offset);
+    } else {
+      // Global variable
+      printf("  lea %s(%%rip), %%rax\n", node->var->name);
+    }
 
     return;
 
@@ -245,9 +251,19 @@ static void assign_lvar_offsets(Obj *prog) {
   }
 }
 
-void codegen(Obj *prog) {
-  assign_lvar_offsets(prog);
+static void emit_data(Obj *prog) {
+  for (Obj *var = prog; var; var = var->next) {
+    if (var->is_function)
+      continue;
 
+    printf("  .data\n");
+    printf("  .globl %s\n", var->name);
+    printf("%s:\n", var->name);
+    printf("  .zero %d\n", var->ty->size);
+  }
+}
+
+static void emit_text(Obj *prog) {
   for (Obj *fn = prog; fn; fn = fn->next) {
     if (!fn->is_function)
       continue;
@@ -276,4 +292,10 @@ void codegen(Obj *prog) {
     printf("  pop %%rbp\n");
     printf("  ret\n");
   }
+}
+
+void codegen(Obj *prog) {
+  assign_lvar_offsets(prog);
+  emit_data(prog);
+  emit_text(prog);
 }
